9bab4150f705bc219fa79c65b37379d0384ed2ea
Marc Jou <smyslov@gmail.com>
2023-03-25 21:00:50 +0100

    Add channels

--- src/isjustok/core.clj
+++ src/isjustok/core.clj
@@ -1,6 +1,8 @@
 (ns isjustok.core
   (:require [clojure.data.json :as json]
             [clj-http.client :as client]
+            [clj-http.conn-mgr :as conn]
+            [clj-http.core :as core]
             [clojure.core.async :as async :refer [<! >! <!! >!! chan go]])
 
   (:gen-class))
@@ -34,37 +36,49 @@
 (defn get-city-temp
   "calls API, if 200 extracts temperature,
    retries on 429"
-  [city]
+  [city {:keys [cm hclient] :as hparams}]
   (let [city (get-coords city)
         {:keys [status body]} (client/get base-url {:query-params {:latitude (:latitude city)
                                                                    :longitude (:longitude city)
                                                                    :current_weather true}
                                                     :accept :json
-                                                    :throw-exceptions false})]
+                                                    :throw-exceptions false
+                                                    :connection-manager cm
+                                                    :http-client hclient})]
     
     (case status
       200 (get-temp body)
       400 "💣"
       429 (do
             (Thread/sleep (* 10 1000))
-            (recur city)))))
+            (recur city hparams)))))
 
 (defn format-city [[city temp]]
   (format "%s: %s°" city temp))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-
+(defn by-channels []
   (let [c (chan)
-        all-cities (keys DB)]
+        ;;all-cities (keys DB)
+        all-cities ["Madrid" "Paris" "London"]
+        cm (conn/make-reusable-conn-manager {:threads 10
+                                             :default-per-route 5})
+        hclient (core/build-http-client {} false cm)]
     (println "Quering for temperatures... ")
     (doseq [city all-cities]
       (go
-        (>! c [city (get-city-temp city)])))
+        (>! c [city (get-city-temp city {:cm cm :hclient hclient})])))
 
+    (println "All requests sent")
     (doseq [_ all-cities]
+      
       (println (format-city (<!! c))))))
+
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (by-channels)
+  )
 
 
 (comment
